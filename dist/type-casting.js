734566468c094937d2bfae4d24f4582772ab234d
Mahakaal007 <abhishek.pes2016@gmail.com>
2020-07-08 11:43:02 +0530

    type-casting program

---
+++ dist/type-casting.js
@@ -0,0 +1,5 @@
+"use strict";
+let count;
+count = 10;
+const num = count;
+console.log(num);
---
+++ src/type-casting.ts
@@ -0,0 +1,5 @@
+let count: number;
+count = 10;
+const num: string = <string>(<unknown>count);
+
+console.log(num);
